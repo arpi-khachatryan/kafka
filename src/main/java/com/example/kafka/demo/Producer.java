b1e7fb2dd2eb8a3ca1d1887e5f015d65925032a9
arpikhachatryan <arpikhachatryan1@gmail.com>
2023-01-17 15:30:41 +0400

    initial commit

--- src/main/java/com/example/kafka/demo/Producer.java
+++ src/main/java/com/example/kafka/demo/Producer.java
@@ -1,2 +1,39 @@
-package com.example.kafka.demo;public class Producer {
+package com.example.kafka.demo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+
+public class Producer {
+    public static void main(String[] args) throws InterruptedException {
+        Logger logger = LoggerFactory.getLogger(Producer.class);
+
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<Integer, String> producer = new KafkaProducer<>(properties);
+        for (int i = 0; i < 1000; i++) {
+            ProducerRecord<Integer, String> record = new ProducerRecord<>("demo-topic", i % 2, "value" + i);
+            producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    logger.info("received new metadata, topic " + metadata.topic() + " partition " +
+                            metadata.partition() + " offsets " + metadata.offset() + " time " + metadata.timestamp());
+                } else {
+                    logger.error("error producing", exception);
+                }
+            });
+            Thread.sleep(1000);
+        }
+        producer.flush();
+        producer.close();
+    }
 }
--- src/main/java/com/example/kafka/example/Producer.java
+++ src/main/java/com/example/kafka/example/Producer.java
@@ -1,2 +1,38 @@
-package com.example.kafka.example;public class Producer {
+package com.example.kafka.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) throws InterruptedException {
+        Logger logger = LoggerFactory.getLogger(Producer.class);
+
+        System.out.println("test");
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        for (int i = 0; i < 10000; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>("demo-topic1", "key_" + i, "value" + i);
+            producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    logger.info("received new metadata, topic " + metadata.topic() + " partition " +
+                            metadata.partition() + " offsets " + metadata.offset() + " time " + metadata.timestamp());
+                } else {
+                    logger.error("error producing", exception);
+                }
+            });
+            Thread.sleep(1000);
+        }
+        producer.flush();
+        producer.close();
+    }
 }
